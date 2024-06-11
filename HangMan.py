f7dd36d4c946c44ac407d8e0419ffc51907954dc
Abhishek chand <abhishekchand2001@gmail.com>
2024-06-11 22:03:38 +0530

    Update HangMan.py
    refined

--- HangMan.py
+++ HangMan.py
@@ -1,10 +1,15 @@
 import random
 Word_List = ["tiger","elephant","camel"]
 pick_word = random.choice(Word_List)
+print(pick_word)
 checkLen = len(pick_word) * 2
 pick_word = list(pick_word)
+temp = pick_word
 newWord = []
 
+for i in range(0,len(pick_word)):
+    newWord.append("_")
+print(newWord)
 while checkLen > 0 :
     UserInput = input("Enter the letter : ")
     if len(UserInput ) > 1:
@@ -12,18 +17,17 @@ while checkLen > 0 :
     
     # if pick_word.find(UserInput):
     #     newWord.
+    
     if UserInput in pick_word :
         loc = pick_word.index(UserInput)
-        newWord.insert(loc,UserInput)
-        pick_word.remove(UserInput)
+        pick_word[loc]="_"
+        if UserInput not in newWord[loc]:
+        # newWord.replace(loc,UserInput)
+            newWord[loc]=UserInput
     elif UserInput not in pick_word :
         print("Not in list you missed another chance")
-    for i in newWord :
-        print(i)    
+    print(newWord)
     checkLen -= 1 
-    if len(pick_word) == 0:
+    if "_" not in newWord:
+        print("You Won")
         checkLen = 0
-if len(pick_word) == 0:
-    print("You Win")
-else :
-    print("You loose")
