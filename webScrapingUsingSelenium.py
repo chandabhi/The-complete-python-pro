29d025bbcc150c1d004343a2aed9f53fac7098bf
Abhishek chand <abhishekchand2001@gmail.com>
2024-06-18 23:32:49 +0530

    Create webScrapingUsingSelenium.py
    webScraping

---
+++ webScrapingUsingSelenium.py
@@ -0,0 +1,29 @@
+# Author: Ardit Sulce, Automate Everything with Python, Udemy
+# Course URL: https://www.udemy.com/course/automate-everything-with-python/
+
+from selenium import webdriver
+from selenium.webdriver.commom.by import By
+from selenium.webdriver.firefox.options import Options 
+
+driver = webdriver.Chrome(options=Options)
+
+def get_drvier():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver = get_drvier()
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
