a49611758be4025f2e7bca165896b6e9cc6a9482
Abhishek chand <abhishekchand2001@gmail.com>
2024-06-28 23:44:53 +0530

    did changes

---
+++ TurtlePrograms/TurtleSquare.py
@@ -0,0 +1,16 @@
+#####Turtle Intro######
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)   
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+
+######## Challenge 1 - Draw a Square ############
